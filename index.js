01edb75e84e7dd64df7b69453e1e0a7b7745da1a
noashabtay313 <84012631+noashabtay313@users.noreply.github.com>
2021-09-05 20:35:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get("/env", (req, res) => {
   res.send(process.env);
 });
 app.get("/", (req, res) => {
-  res.send(process.env.MESSAGE || "HELLO WORLD");
+  res.send(process.env.MESSAGE || "Page reached");
 });
 
 app.listen(port, () => {
